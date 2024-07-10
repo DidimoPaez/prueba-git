7a7f3d50362883b061690dc37090df8b622139d7
ricadolcvuba <132418140+ricadolcvuba@users.noreply.github.com>
2024-07-10 20:52:31 -0300

    c

--- src/prueba_git/core.clj
+++ src/prueba_git/core.clj
@@ -1,14 +1,21 @@
 (ns prueba-git.core)
 
 (defn foo
-  "I don't do a whole lot."
+  "I don't do a whole lot. hola"
   [x]
   (println x "Hello, World!"))
 (defn fibonacci [n]
   (if (< n 2)
     1
     (* (fibonacci (n - 1)) (fibonacci (n - 1)))))
+
 (defn foo
       "I don't know what to do"
       [x]
       (println x "Hello, MUNDO!"))
+
+(defn suma
+  "I don't do a whole lot. hola"
+  [x]
+  (println x "Hello, World!"))
+
